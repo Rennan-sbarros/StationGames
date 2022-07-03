168d1d45b18639bcaca6ccad4037e564dd2aabfe
Aquiles <aquilesfalcaoo@gmail.com>
2022-07-02 21:47:52 -0300

    feat: finalizado implementação do header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    colors: {
-      'purple': '#1C1633',
-      'purple-bright': '#442E99'
-    }
+    extend: {
+      fontFamily: {
+        Poppins: ["Poppins"],
+      },
+      colors: {
+        purple: "#1C1633",
+        "purple-bright": "#442E99",
+      },
+    },
   },
   plugins: [],
-}
+};
