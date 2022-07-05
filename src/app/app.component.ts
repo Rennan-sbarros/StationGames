a2313f8ad1c421b7ec4123453e71e006721096a6
Aquiles <aquilesfalcaoo@gmail.com>
2022-07-05 10:01:30 -0300

    feat: adicionado lazy loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,4 @@
-import { Games } from './shared/models/games';
 import { Component, OnInit } from '@angular/core';
-import { GamesService } from './core/services/games.service';
-import SwiperCore, { Autoplay } from 'swiper';
-
-SwiperCore.use([Autoplay]);
 
 @Component({
   selector: 'app-root',
@@ -11,25 +6,5 @@ SwiperCore.use([Autoplay]);
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  public games!: Games[];
-
-  constructor(private gamesService: GamesService) {}
-
-  ngOnInit(): void {
-    this.gamesList();
-  }
-
-  gamesList() {
-    this.gamesService.getGames().subscribe((result: any) => {
-      this.games = result?.results;
-    });
-  }
-
-  onSwiper([swiper]: any) {
-    console.log(swiper);
-  }
-
-  onSlideChange() {
-    console.log('slide change');
-  }
+  ngOnInit(): void {}
 }
---
+++ src/app/pages/home/home.module.ts
@@ -0,0 +1,13 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './components/home.component';
+import { HomeRoutingModule } from './home-routing.module';
+import { SwiperModule } from 'swiper/angular';
+
+@NgModule({
+  declarations: [HomeComponent],
+  imports: [CommonModule, HomeRoutingModule, SwiperModule],
+  exports: [HomeComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+})
+export class HomeModule {}
